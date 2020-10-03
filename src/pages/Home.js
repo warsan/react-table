6a0780a4088e52839691ff38125cc4b408f474f5
Сандро <warsan00@mail.ru>
2020-10-04 01:39:05 +0500

    Правки

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import AddIcon from "@material-ui/icons/Add";
+
 import Icon from "@material-ui/core/Icon";
-import DeleteIcon from "@material-ui/icons/Delete";
 import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 
 const styles = theme => ({
   button: { margin: theme.spacing.unit },
